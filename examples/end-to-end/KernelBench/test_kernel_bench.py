455d61cf60b2b553d545783bcb5258d4cf50fe03
Renato Golin <rengolin@systemcall.eu>
2026-05-14 10:34:14 +0100

    BF16 matmul from KernelBench working (#145)
    Performance is twice as F32, which is around 85% of peak, so good first
    step.

--- examples/end-to-end/KernelBench/test_kernel_bench.py
+++ examples/end-to-end/KernelBench/test_kernel_bench.py
@@ -3,6 +3,7 @@
 # REQUIRES: torch
 # REQUIRES: kernel_bench
 
+import argparse
 import re
 import subprocess
 import platform
@@ -13,39 +14,72 @@ project_root = script_path.parent.parent.parent
 kb_program = project_root / "tools" / "kernel_bench"
 kb_default_pipeline = kb_program.parent / "kernel_bench.yaml"
 kb_path = project_root / "third_party" / "KernelBench" / "KernelBench"
-arch = platform.machine()
+
+
+def get_pipeline_file(kernel_name: str, dtype: str) -> Path:
+    """
+    Returns the appropriate pipeline file for a given kernel.
+    """
+    arch = platform.machine()
+    if arch != "x86_64":
+        return kb_default_pipeline
+
+    # Level 1 matmuls should use the same pipelines
+    if kernel_name.startswith("level1") and "matrix_multiplication" in kernel_name:
+        pipeline = script_path / f"schedules/{arch}/matmul/{dtype}.yaml"
+        if pipeline.exists():
+            return pipeline
+
+    # Otherwise, just return the safe option
+    return kb_default_pipeline
+
+
 tests = [
     {
         "kernel": "level1/1_Square_matrix_multiplication_.py",
-        "input_shapes": "1024x1024xf32xrnd,1024x1024xf32xid",
-        "output_shape": "1024x1024xf32x0",
+        "input_shapes": ["1024x1024", "1024x1024"],
+        "initializations": ["rnd", "id"],
+        "output_shape": "1024x1024",
+        "dtypes": ["f32", "bf16"],
         "gflops": (1024 * 1024 * 1024 * 2) / 1e9,
-        "pipeline": f"{script_path}/cpu_matmul.yaml"
-        if arch == "x86_64"
-        else str(kb_default_pipeline),
-    },
-    {
-        "kernel": "level1/1_Square_matrix_multiplication_.py",
-        "input_shapes": "32x32xbf16xrnd,32x32xbf16xid",
-        "output_shape": "32x32xbf16x0",
-        "pipeline": str(kb_default_pipeline),
     },
     {
         "kernel": "level1/2_Standard_matrix_multiplication_.py",
-        "input_shapes": "512x1024xf32xrnd,1024x512xf32xrnd",
-        "output_shape": "512x512xf32x0",
+        "input_shapes": ["512x1024", "1024x512"],
+        "initializations": ["rnd", "rnd"],
+        "output_shape": "512x512",
+        "dtypes": ["f32", "bf16"],
         "gflops": (512 * 1024 * 512 * 2) / 1e9,
-        "pipeline": f"{script_path}/cpu_matmul.yaml"
-        if arch == "x86_64"
-        else str(kb_default_pipeline),
-    },
-    {
-        "kernel": "level1/2_Standard_matrix_multiplication_.py",
-        "input_shapes": "8x16xbf16xrnd,16x8xbf16xrnd",
-        "output_shape": "8x8xbf16x0",
-        "pipeline": str(kb_default_pipeline),
     },
 ]
+
+
+def get_tests(args: argparse.Namespace) -> list[dict]:
+    """
+    Returns the list of tests to be executed.
+    """
+    test_list = []
+    for test in tests:
+        for dtype in test["dtypes"]:
+            if not args.bf16 and dtype == "bf16":
+                continue
+            test_list.append(
+                {
+                    "kernel": test["kernel"],
+                    "input_shapes": ",".join(
+                        f"{shape}x{dtype}x{init}"
+                        for shape, init in zip(
+                            test["input_shapes"], test["initializations"]
+                        )
+                    ),
+                    "output_shape": f"{test['output_shape']}x{dtype}x0",
+                    "gflops": test["gflops"]
+                    if "gflops" in test and args.benchmark
+                    else None,
+                    "pipeline": str(get_pipeline_file(test["kernel"], dtype)),
+                }
+            )
+    return test_list
 
 
 def get_flops_per_second(stdout: str, gflops: float) -> float:
@@ -58,7 +92,22 @@ def get_flops_per_second(stdout: str, gflops: float) -> float:
 
 
 if __name__ == "__main__":
-    for test in tests:
+    Parser = argparse.ArgumentParser(
+        description="""Kernel Bench testing & benchmarking."""
+    )
+    Parser.add_argument(
+        "--benchmark",
+        action=argparse.BooleanOptionalAction,
+        help="Whether to run the benchmark.",
+    )
+    Parser.add_argument(
+        "--bf16",
+        action=argparse.BooleanOptionalAction,
+        help="Enable bf16 precision kernels.",
+    )
+    args = Parser.parse_args()
+
+    for test in get_tests(args):
         kb_kernel = kb_path / test["kernel"]
         command_line = [
             str(kb_program),
@@ -72,7 +121,8 @@ if __name__ == "__main__":
             "--print-tensor=1",
             "--seed=42",
         ]
-        if "gflops" in test:
+        benchmark = test.get("gflops") is not None
+        if benchmark:
             command_line += ["--benchmark"]
         print(f"Running command: {' '.join(command_line)}")
         result = subprocess.run(
@@ -83,7 +133,7 @@ if __name__ == "__main__":
 
         print("STDOUT:")
         print(result.stdout)
-        if "gflops" in test:
+        if benchmark:
             flops_per_second = get_flops_per_second(result.stdout, test["gflops"])
             if flops_per_second > 0:
                 print(f"Performance: {flops_per_second:.2f} GFLOPS")
@@ -96,25 +146,11 @@ if __name__ == "__main__":
 # CHECK: 1_Square_matrix_multiplication_.mlir
 # CHECK: 0.3745{{.*}} 0.9507{{.*}} 0.7319{{.*}} ... 0.2973{{.*}} 0.9243{{.*}} 0.9710{{.*}}
 # CHECK: 0.7201{{.*}} 0.9926{{.*}} 0.1208{{.*}} ... 0.1742{{.*}} 0.3485{{.*}} 0.6436{{.*}}
-# CHECK: Performance: {{.*}} GFLOPS
-
-# CHECK-NOT: Execution failed
-
-# CHECK: 1_Square_matrix_multiplication_.mlir
-# CHECK: 0.375 0.949219 0.730469 ... 0.0463867 0.609375 0.170898
-# CHECK: 0.271484 0.589844 0.361328 ... 0.296875 0.925781 0.972656
 
 # CHECK-NOT: Execution failed
 
 # CHECK: 2_Standard_matrix_multiplication_.mlir
 # CHECK: 249.78{{.*}} 260.13{{.*}} 249.36{{.*}} ... 261.10{{.*}} 260.49{{.*}} 257.09{{.*}}
 # CHECK: 243.56{{.*}} 250.91{{.*}} 252.38{{.*}} ... 260.40{{.*}} 261.56{{.*}} 256.24{{.*}}
-# CHECK: Performance: {{.*}} GFLOPS
-
-# CHECK-NOT: Execution failed
-
-# CHECK: 2_Standard_matrix_multiplication_.mlir
-# CHECK: 3.125 3.76562 4.53125 4.40625 4.4375 3.26562 3.53125 3.9375
-# CHECK: 5.03125 5.3125 5.8125 4.8125 4.75 4.34375 5.3125 5.5625
 
 # CHECK-NOT: Execution failed
